9f57e331076d552970d30fe6f928ed0f590309ad
adevinwild <68734155+adevinwild@users.noreply.github.com>
2025-10-01 07:24:33 +0200

    chore: remove configuration files for Prettier, ESLint, and Jest

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  transform: { "^.+\\.[jt]s?$": "@swc/jest" },
-  testEnvironment: `node`,
-  moduleFileExtensions: [`js`, `ts`],
-  testMatch: ["**/__tests__/**/*.spec.[jt]s"],
-}
